b7f69d41bb3996642d93504634be1d543aa85fe4
Reese <connorreese@MacBook-Pro-7.local>
2017-10-15 16:10:19 -0600

    Here is my submission, if you would like to see more test cases let me know and I can create more

--- nightwatch/test_data/test_data.js
+++ nightwatch/test_data/test_data.js
@@ -57,6 +57,218 @@ module.exports = {
                 queryTitle: 'No results generated due to error.',
                 assembledQuery: ''
             }
-        }
+        },
+          
+        fullEntry: {
+                //the 'key' for the fields should match the key of the selectors in css_selectors
+                fields: {
+                    hdr: '123456789',
+                    mke: 'MKE',
+                    oai: 'CHI1234SI',
+                    nam: 'Harry Dresden',
+                    sex: 'M',
+                    rac: 'W',
+                    hgt: '607',
+                    wgt: '200',
+                    hai: 'Brown',
+                    off: 'Arson',
+                    dow: '05022016',
+                    oln: '',
+                    ols: '',
+                    oly: '',
+                    lic: 'W493GK',
+                    lis: 'UT',
+                    liy: '2016'
+                },
+                results: {
+                    header: 'Valid',
+                    errorList: {},
+                    queryTitle: 'Assembled Query:',
+                    assembledQuery: '123456789.MKE.CHI1234SI.Harry Dresden.M.W.607.200.Brown.Arson.05022016....W493GK.UT.2016'
+                }
+            },
+   
+            maximumCharacterEntry: {
+                //the 'key' for the fields should match the key of the selectors in css_selectors
+                fields: {
+                    hdr: '1234567891234567891',
+                    mke: 'MKES',
+                    oai: 'CHI1234SI',
+                    nam: 'Harry ABCDEFGHIJKLMNOP Dresden',
+                    sex: 'M',
+                    rac: 'W',
+                    hgt: '607',
+                    wgt: '200',
+                    hai: 'Brownbrown',
+                    off: 'Serial arsonist',
+                    dow: '05022016',
+                    oln: '',
+                    ols: '',
+                    oly: '',
+                    lic: 'W493GKSS',
+                    lis: 'UT',
+                    liy: '2016'
+                },
+                results: {
+                    header: 'Valid',
+                    errorList: {},
+                    queryTitle: 'Assembled Query:',
+                    assembledQuery: '1234567891234567891.MKES.CHI1234SI.Harry ABCDEFGHIJKLMNOP Dresden.M.W.607.200.Brownbrown.Serial arsonist.05022016....W493GKSS.UT.2016'
+                }
+            },
+
+            minimumCharacterEntry: {
+                //the 'key' for the fields should match the key of the selectors in css_selectors
+                fields: {
+                    hdr: '123456789',
+                    mke: 'MK',
+                    oai: 'CHI1234SI',
+                    nam: 'Ron',
+                    sex: 'M',
+                    rac: 'W',
+                    hgt: '607',
+                    wgt: '200',
+                    hai: 'Red',
+                    off: 'Arson',
+                    dow: '05022016',
+                    oln: '',
+                    ols: '',
+                    oly: '',
+                    lic: 'W493G',
+                    lis: 'UT',
+                    liy: '2016'
+                },
+                results: {
+                    header: 'Valid',
+                    errorList: {},
+                    queryTitle: 'Assembled Query:',
+                    assembledQuery: '123456789.MK.CHI1234SI.Ron.M.W.607.200.Red.Arson.05022016....W493G.UT.2016'
+                }
+            },
+
+            dateBefore1900: {
+                //the 'key' for the fields should match the key of the selectors in css_selectors
+                fields: {
+                    hdr: '123456789',
+                    mke: 'MKE',
+                    oai: 'CHI1234SI',
+                    nam: 'Harry Dresden',
+                    sex: 'M',
+                    rac: 'W',
+                    hgt: '607',
+                    wgt: '200',
+                    hai: 'Brown',
+                    off: 'Arson',
+                    dow: '05021899',
+                    oln: '',
+                    ols: '',
+                    oly: '',
+                    lic: '',
+                    lis: '',
+                    liy: ''
+                },
+                results: {
+                    header: 'Errors Received:',
+                    errorList: {
+                        dow: `The "Date of Warrant/Violation" field must be entered as a date, MMDDYYYY, no earlier than 01011900 and no later than today's date.`
+                    },
+                    queryTitle: 'No results generated due to error.',
+                    assembledQuery: ''
+                }
+            },
+
+            dateInFuture: {
+                //the 'key' for the fields should match the key of the selectors in css_selectors
+                fields: {
+                    hdr: '123456789',
+                    mke: 'MKE',
+                    oai: 'CHI1234SI',
+                    nam: 'Harry Dresden',
+                    sex: 'M',
+                    rac: 'W',
+                    hgt: '607',
+                    wgt: '200',
+                    hai: 'Brown',
+                    off: 'Arson',
+                    dow: '05022020',
+                    oln: '',
+                    ols: '',
+                    oly: '',
+                    lic: '',
+                    lis: '',
+                    liy: ''
+                },
+                results: {
+                    header: 'Errors Received:',
+                    errorList: {
+                        dow: `The "Date of Warrant/Violation" field must be entered as a date, MMDDYYYY, no earlier than 01011900 and no later than today's date.`
+                    },
+                    queryTitle: 'No results generated due to error.',
+                    assembledQuery: ''
+                }
+            },
+
+            sexCharacterError: {
+                //the 'key' for the fields should match the key of the selectors in css_selectors
+                fields: {
+                    hdr: '123456789',
+                    mke: 'MKE',
+                    oai: 'CHI1234SI',
+                    nam: 'Harry Dresden',
+                    sex: 'x',
+                    rac: 'W',
+                    hgt: '607',
+                    wgt: '200',
+                    hai: 'Brown',
+                    off: 'Arson',
+                    dow: '05022016',
+                    oln: '',
+                    ols: '',
+                    oly: '',
+                    lic: '',
+                    lis: '',
+                    liy: ''
+                },
+                results: {
+                    header: 'Errors Received:',
+                    errorList: {
+                        sex: `The "Sex" field must be entered in as a single character, M for male, F for female, U for unknown.`
+                    },
+                    queryTitle: 'No results generated due to error.',
+                    assembledQuery: ''
+                }
+            },
+
+            missingRequiredField: {
+                //the 'key' for the fields should match the key of the selectors in css_selectors
+                fields: {
+                    hdr: '123456789',
+                    mke: 'MKE',
+                    oai: 'CHI1234SI',
+                    nam: 'Harry Dresden',
+                    sex: 'M',
+                    rac: 'W',
+                    hgt: '607',
+                    wgt: '',
+                    hai: 'Brown',
+                    off: 'Arson',
+                    dow: '05022016',
+                    oln: '12345',
+                    ols: '',
+                    oly: '',
+                    lic: '',
+                    lis: '',
+                    liy: ''
+                },
+                results: {
+                    header: 'Errors Received:',
+                    errorList: {
+                        wgt: `The field named "Weight" must be included.`
+                    },
+                    queryTitle: 'No results generated due to error.',
+                    assembledQuery: ''
+                }
+            },
+            
     }
 }
--- nightwatch/tests/exampleTest.js
+++ nightwatch/tests/exampleTest.js
@@ -27,6 +27,7 @@ module.exports = {
             //expect assembled query to be right
         browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
     },
+    
     'I can put bad data in and get the right error message' : browser => {
             //set the transaction from your data file
         let transaction = data.transactions.olnOnly
@@ -44,5 +45,140 @@ module.exports = {
         browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
             //expect assembled query to be right
         browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
-    }
+    },
+
+    'I can put data in ALL of the fields and generate a result' : browser => {
+        //set the transaction from your data file
+    let transaction = data.transactions.fullEntry
+        //send the fields & data to input
+    functions.inputSet(selectors.fields, transaction.fields, browser)
+    browser
+        //submit
+        .click(selectors.buttons.submit)
+        .pause(100)
+        //expect header to be right
+        .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
+        //expect error list to be empty
+    browser.expect.element(selectors.messages.errorList).text.to.equal('')
+        //expect query title to be right
+    browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
+        //expect assembled query to be right
+    browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+    },
+
+    'I can put the maximum character count in each entry' : browser => {
+        //set the transaction from your data file
+    let transaction = data.transactions.maximumCharacterEntry
+        //send the fields & data to input
+    functions.inputSet(selectors.fields, transaction.fields, browser)
+    browser
+        //submit
+        .click(selectors.buttons.submit)
+        .pause(100)
+        //expect header to be right
+        .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
+        //expect error list to be empty
+    browser.expect.element(selectors.messages.errorList).text.to.equal('')
+        //expect query title to be right
+    browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
+        //expect assembled query to be right
+    browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+
+    },
+
+    'I can put the minimum character count in each entry' : browser => {
+        //set the transaction from your data file
+    let transaction = data.transactions.minimumCharacterEntry
+        //send the fields & data to input
+    functions.inputSet(selectors.fields, transaction.fields, browser)
+    browser
+        //submit
+        .click(selectors.buttons.submit)
+        .pause(100)
+        //expect header to be right
+        .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
+        //expect error list to be empty
+    browser.expect.element(selectors.messages.errorList).text.to.equal('')
+        //expect query title to be right
+    browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
+        //expect assembled query to be right
+    browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+    },
+
+    'I can put in a date before 1900 and get the correct error message' : browser => {
+        //set the transaction from your data file
+    let transaction = data.transactions.dateBefore1900
+        //send the fields & data to input
+    functions.inputSet(selectors.fields, data.transactions.dateBefore1900.fields, browser)
+    browser
+        //submit
+        .click(selectors.buttons.submit)
+        .pause(100)
+        //expect header to be right
+        .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
+        //expect error list to contain all the right errors
+    functions.messagesCheck(selectors.messages.errorList, transaction.results.errorList, browser)
+        //expect query title to be right
+    browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
+        //expect assembled query to be right
+    browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+},
+
+'I can put in a date in the future and get the correct error message' : browser => {
+    //set the transaction from your data file
+let transaction = data.transactions.dateInFuture
+    //send the fields & data to input
+functions.inputSet(selectors.fields, data.transactions.dateInFuture.fields, browser)
+browser
+    //submit
+    .click(selectors.buttons.submit)
+    .pause(100)
+    //expect header to be right
+    .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
+    //expect error list to contain all the right errors
+functions.messagesCheck(selectors.messages.errorList, transaction.results.errorList, browser)
+    //expect query title to be right
+browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
+    //expect assembled query to be right
+browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+},
+
+'I can put in an invalid sex character and get the right error message' : browser => {
+    //set the transaction from your data file
+let transaction = data.transactions.sexCharacterError
+    //send the fields & data to input
+functions.inputSet(selectors.fields, data.transactions.sexCharacterError.fields, browser)
+browser
+    //submit
+    .click(selectors.buttons.submit)
+    .pause(100)
+    //expect header to be right
+    .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
+    //expect error list to contain all the right errors
+functions.messagesCheck(selectors.messages.errorList, transaction.results.errorList, browser)
+    //expect query title to be right
+browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
+    //expect assembled query to be right
+browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+},
+
+'I can neglect a required entry and get the right error message' : browser => {
+    //set the transaction from your data file
+let transaction = data.transactions.missingRequiredField
+    //send the fields & data to input
+functions.inputSet(selectors.fields, data.transactions.missingRequiredField.fields, browser)
+browser
+    //submit
+    .click(selectors.buttons.submit)
+    .pause(100)
+    //expect header to be right
+    .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
+    //expect error list to contain all the right errors
+functions.messagesCheck(selectors.messages.errorList, transaction.results.errorList, browser)
+    //expect query title to be right
+browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
+    //expect assembled query to be right
+browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+},
+
 }
